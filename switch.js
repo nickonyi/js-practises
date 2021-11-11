13da9cb2772f183ff3a6617230420f60e16389db
nickonyi <nickoluoch1130@gmail.com>
2021-11-11 21:30:41 +0300

    another push

---
+++ switch.js
@@ -0,0 +1,26 @@
+const day = new Date().getDay();
+
+switch (day) {
+    case 0:
+        console.log("it's sunday time to relax");
+        break;
+    case 1:
+        console.log("it's monday time to work");
+        break;
+    case 2:
+        console.log("it's tuesday you got this");
+        break;
+    case 3:
+        console.log("Hump day already");
+        break;
+    case 4:
+        console.log("just one more day till weekend");
+        break;
+    case 5:
+        console.log("happy friday");
+        break;
+        ca
+    case 6:
+        console.log("have a wonderful saturday");
+        break;
+}
